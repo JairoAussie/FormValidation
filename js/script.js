aef9c426fe02abc011e3df8e0d431d41ddf7b7aa
Jairo Bilbao <jairoaussie@gmail.com>
2019-06-18 14:17:01 +0200

    Add some comments

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
-//console.log('Hello');
 //When the page first loads, the first text field should be in focus by default.
 $('#name').focus();
 //Include a text field that will be revealed when the "Other" option is selected from the "Job Role" drop down menu.
 $('#other-title').hide();
+//Show/hide the input depending on 'other' is selected or not
 $('#title').change(function() {
     if ($(this).val() === 'other') {
         $('#other-title').show();
@@ -10,27 +10,27 @@ $('#title').change(function() {
         $('#other-title').hide();
     }
 });
-/*...
-When the form is initially loaded, we need to update the "Design" and "Color" fields so that it's
-clear to the user that they need to select a theme before selecting a color. Use jQuery to:
-● Hide the “Select Theme” `option` element in the “Design” menu.
-● Update the “Color” field to read “Please select a T-shirt theme”.
-● Hide the colors in the “Color” drop down menu.
-● NOTE: Be sure to check out the helpful links in the second section of this Study Guide if
-you’re unsure of how to accomplish these steps.
 
-...*/
+//T-SHIRT SECTION
+
+//Hide the “Select Theme” `option` element in the “Design” menu.
 $('#design').find("option").eq(0).hide();
+//Update the “Color” field to read “Please select a T-shirt theme”.
 $('#colors-js-puns label').text("Please select a T-shirt theme.");
+//Hide the colors in the “Color” drop down menu.
 $('#color').hide();
 
+//event that controls when a design option has been changed
 $('#design').change(function() {
+    //update color label and show the options
     $('#colors-js-puns label').text("Color");
     $('#color').show();
-    //$(this).find("option").eq(0).hide();
+    //If JS Puns design is selected
     if ($(this).val() === 'js puns') {
+        //Select the firts option
         $('#color option').eq(0).attr('selected', true);
-        $("#color > option").each(function() {
+        //iterate the options and ask if the text contains 'js puns', according to this answer hide or show the option
+        $("#color option").each(function() {
             if (this.text.toLowerCase().indexOf("js puns")>=0){
                 $(this).show();
             } else {
@@ -38,9 +38,12 @@ $('#design').change(function() {
             }
             
         });
-    } else if ($(this).val() === 'heart js'){
+    } //If Heart JS design is selected
+    else if ($(this).val() === 'heart js'){
+        //Tomato will be the selected option
         $('#color option').eq(3).attr('selected', true);
-        $("#color > option").each(function() {
+        //the same iteration but doing the opposite according to the contains 'js puns'.
+        $("#color option").each(function() {
             if (this.text.toLowerCase().indexOf("js puns")>=0){
                 $(this).hide();
             } else {
@@ -50,3 +53,5 @@ $('#design').change(function() {
         });
     }
 });
+
+//ACTIVITIES SECTION
